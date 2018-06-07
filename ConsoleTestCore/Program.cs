a9b4942254c5b85fb8a45b2d6accc37d83b946e3
Eduardo Linares Peñailillo <eduardolinaresp@gmail.com>
2018-06-07 01:26:43 -0400

    inicio

---
+++ ConsoleTestCore/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleTestCore.DAL;
+using ConsoleTestCore.Models;
+using System;
+
+namespace ConsoleTestCore
+{
+    class Program
+    {
+        
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            try
+            {
+                UsuarioDAL _UserDAL = new UsuarioDAL();
+
+                Usuario User = _UserDAL.Read("patricio.m");
+
+                Console.WriteLine(User.Nombre);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+            Console.ReadKey();
+
+        }
+
+    
+    }
+}
